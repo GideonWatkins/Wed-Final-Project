4663c774c08445b9f92b6d10adfb8ee01e7be01e
Gideon Watkins <gideon.watkins@students.snow.edu>
2024-04-25 12:25:55 -0600

    again again

--- docs/service.js
+++ docs/service.js
@@ -1,7 +1,11 @@
+// const url = "http://localhost:5274/books";
+const url = "http://localhost:5274/books";
 export const GetAllBooks = async () => {
-    const url = "http://localhost:5274/books";
     const response = await fetch(url);
     return await response.json();
 }
+
+
+
 
 //500 error messages mean that you accessed a server and received a response, albeit an angry one
